3f336aedd1f123f8f9e0c4d697279f7e224e2077
Yoshihara Fumio <yoshihara@un-t.com>
2019-10-20 16:26:11 +0900

    リストアイテムのスタイリング（途中）

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="template-inner">
     <div class="inputArea">
-      <input class="inputArea_input" type="text" />
-      <button>btn</button>
+      <input
+        class="inputArea_inputform"
+        placeholder="Enter activity…"
+        type="text"
+      />
+      <button class="inputArea_inputbtn">btn</button>
     </div>
 
     <ul class="todoList">
-      <li class="todoList_item">Do loundry</li>
-      <li class="todoList_item">Clean kithen</li>
-      <li class="todoList_item">Cook dinner</li>
+      <li class="todoList_item">
+        <button class="todoList_check">○</button>
+        <p class="todoList_txt">Do loundry</p>
+        <button class="todoList_delete">ゴミ</button>
+      </li>
+
+      <li class="todoList_item">
+        <button class="todoList_check">○</button>
+        <p class="todoList_txt">Clean kithen</p>
+        <button class="todoList_delete">ゴミ</button>
+      </li>
+      <li class="todoList_item">
+        <button class="todoList_check">○</button>
+        <p class="todoList_txt">Cook dinner</p>
+        <button class="todoList_delete">ゴミ</button>
+      </li>
     </ul>
   </div>
 </template>
@@ -26,20 +43,60 @@ export default {
 }
 
 .inputArea {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
-  height: 56px;
+  /*height: 56px;*/
   border-radius: 10px;
   background-color: #4bc9d0;
+  padding: 10px 20px;
 }
 
-.inputArea_input {
+.inputArea_inputform {
+  flex: 1;
+  margin-right: 10px;
+  padding: 10px 0;
   background-color: transparent;
-  padding: 20px;
+  font-size: 15px;
+  color: #ffffff;
+  /*background: red;*/
+}
+
+.inputArea_inputform::placeholder {
+  color: #ffffff;
+}
+
+.inputArea_inputbtn {
+  width: 28px;
+  height: 28px;
+  background: tomato;
+  /*object-fit: contain;*/
 }
 
 .todoList {
 }
 
 .todoList_item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 14px;
+}
+
+.todoList_check {
+  width: 24px;
+  height: 24px;
+}
+
+.todoList_txt {
+  flex: 1;
+  padding: 0 20px;
+  font-size: 15px;
+}
+
+.todoList_delete {
+  width: 15px;
+  height: 19.4px;
 }
 </style>

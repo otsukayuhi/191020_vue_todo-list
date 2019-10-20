ae889f066fdaa7673a4fcdfd3230dd9511bf1e31
家田晋輔 <>
2019-10-20 15:50:27 +0900

    家田：デザイン作業中

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Todolist />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Todolist from "./components/Todolist.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Todolist
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+
+input {
+  border: 0;
 }
 </style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="template-inner">
+    <div class="inputArea">
+      <input class="inputArea_input" type="text" />
+      <button>btn</button>
+    </div>
+
+    <ul class="todoList">
+      <li class="todoList_item">Do loundry</li>
+      <li class="todoList_item">Clean kithen</li>
+      <li class="todoList_item">Cook dinner</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.template-inner {
+  padding: 40px 20px;
+}
+
+.inputArea {
+  width: 100%;
+  height: 56px;
+  border-radius: 10px;
+  background-color: #4bc9d0;
+}
+
+.inputArea_input {
+  background-color: transparent;
+  padding: 20px;
+}
+
+.todoList {
+}
+
+.todoList_item {
+}
+</style>
